9be1ec2520bb1b78c9da50e2cb995efc1f2f719d
Servet Lapardhaja <60490284+lapardhaja@users.noreply.github.com>
2025-03-11 10:58:52 -0400

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-    var sidebar = document.querySelector('.mobile-sidebar');
-    if (sidebar.style.left === "0px") {
-        sidebar.style.left = "-250px";
-    } else {
-        sidebar.style.left = "0px";
-    }
-}
